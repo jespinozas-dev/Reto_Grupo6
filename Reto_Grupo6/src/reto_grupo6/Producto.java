c4f395691086878516de37a35921e5a2b6cec92d
J.ESPINOZA <J.ESPINOZA@PC16-A14.CEMK.LOCAL>
2026-02-20 09:53:07 +0100

    clase producto terminada

---
+++ Reto_Grupo6/src/reto_grupo6/Producto.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package reto_grupo6;
+
+/**
+ *
+ * @author J.ESPINOZA
+ */
+public class Producto {
+    private String nombre;
+    private double precio;
+    private int unidades;
+    
+
+public Producto(String nombre, double precio, int unidades){
+    this.nombre=nombre;
+    this.precio=precio;
+    this.unidades=unidades;
+}
+
+public String getNombre(){
+    return nombre;
+}
+
+public double getPrecio(){
+    return precio;
+}
+
+public int getUnidades(){
+    return unidades;
+}
+
+public void setNombre(String nombre){
+    this.nombre=nombre;
+}
+
+public void setPrecio(double precio){
+    this.precio=precio;
+}
+
+public void setUnidades(int unidades){
+    this.unidades=unidades;
+}
+
+public String consultar(){
+    return "Nombre: " + nombre + "\n Precio: " + precio + "\n Unidades: " + unidades;
+}
+
+public String probar(){
+    return "Se prueba el producto";
+}
+    
+}
+
